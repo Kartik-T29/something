c63f15bcfb8d6310313d24d1e5302d597ec3ec21
ektatripathi2000 <ektatripathi2000@gmail.com>
2025-09-24 07:10:03 +0000

    first commit

---
+++ src/register.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpYrLHbhSOOHH-KtINzkLb95A07quRp-M",
+  authDomain: "something-a5f8e.firebaseapp.com",
+  projectId: "something-a5f8e",
+  storageBucket: "something-a5f8e.firebasestorage.app",
+  messagingSenderId: "623627655294",
+  appId: "1:623627655294:web:2348e19d45c028cefa602f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
